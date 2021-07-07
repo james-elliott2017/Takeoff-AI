6ad16ca3562b4bf9af6e991e37522271a948fe77
James Elliott <48301689+james-elliott2017@users.noreply.github.com>
2021-07-07 12:04:19 -0700

    classing (2.0)
    removed original .pys with working class versions

--- Takeoff_Main.py
+++ Takeoff_Main.py
@@ -4,9 +4,8 @@
 #Primary Script to all projects
 #FINAL VERSION --- put all calls here so that javascript can communicate with this file
 
-from OCRLibrary.TextExtractorMain import textExtracMain
 from AutoCount.AutocountMain import count
-from OCRLibrary.TextExtractorMain_Beta import text_counter
+from OCRLibrary.TextExtractorMain import text_counter
 
 
 #FINAL VERSION --- one script for autocount & text_count --> allows shared project name
@@ -23,15 +22,13 @@ working_dir = AutoCount_Class.create_project(project)
 #AutoCount_Class.run_counts(working_dir,is_dir=True)
 
 ###TEXT EXTRACTOR###
-#textExtracMain(project, division)
 text_class = text_counter()
 text_class.textExtracMain(project, division)
 
-###OCR###
-#not existing yet
+###OCR-Extractor###
+# coming soon
 
 ###TO DO LIST###
 #2 Add image_search_dimensions for cropping out edges (AutoMainCount)
 #3 Add text window to ignore edges (TextExtractormain)
-#4 Class(ify) textExtracMain()
 #5 Change CSV formatting to use highlight name instead
