efcb8fc684c69a9e4aac855c5aabd43693d73999
James Elliott <48301689+james-elliott2017@users.noreply.github.com>
2021-07-07 12:30:25 -0700

    Update Takeoff_Main.py

--- Takeoff_Main.py
+++ Takeoff_Main.py
@@ -16,10 +16,10 @@ division = r"Data_Floor"
 
 ###LOAD PROJECT###
 AutoCount_Class = count()
-working_dir = AutoCount_Class.create_project(project)
+working_dir = AutoCount_Class.create_project(project) #works as a loader also
 
 ###AutoCount###
-#AutoCount_Class.run_counts(working_dir,is_dir=True)
+# AutoCount_Class.run_counts(working_dir,is_dir=True)
 
 ###TEXT EXTRACTOR###
 text_class = text_counter()
