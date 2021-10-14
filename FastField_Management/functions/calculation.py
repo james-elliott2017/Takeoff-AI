19c8bcc57163114c0f00ac04d5e202db171180b0
James Elliott <48301689+james-elliott2017@users.noreply.github.com>
2021-10-14 13:45:19 -0700

    Update calculation.py
    sum() function based on job num added

--- FastField_Management/functions/calculation.py
+++ FastField_Management/functions/calculation.py
@@ -2,14 +2,28 @@
 import pandas as pd
 import os
 
-class view_csv:
+class load_csv:
 	def __init__(self,csv_path):
-		dataset = pd.read_csv(csv_path)
-	def view_data(self):
-		pass
+		self.csv_path = csv_path
+		self.dataset = pd.read_csv(csv_path)
+class calculations(load_csv):
+	def __init__(self,csv_path):
+		super().__init__(csv_path)
+	def grab_job_rows(self,job_number,job_num_column_name="Job Number"):
+		"""return rows of given job_number"""
+		job_rows = self.dataset.loc[self.dataset[job_num_column_name] == job_number]
+		return job_rows
+	def sum(self,job_number,col_to_sum):
+		"""sums column based on job number conditional"""
+		job_rows = self.grab_job_rows(job_number)
+		col_sum = job_rows[col_to_sum].sum()
+		return col_sum
 
 def test_main():
-	data_path = r"S:\Personal Folders\Databases\Cleaned_Dataset.csv"
+	data_path = r"S:\Personal Folders\Databases\Cleaned_Dataset_testing.csv"
+	job = 4670
+	calcs = calculations(data_path)
+	calcs.sum(job,"fiber_terminated")
 
 if __name__ == '__main__':
-	main()
+	test_main()
